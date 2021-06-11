f9bd4af380cbfe2b0150953699cd75e16f1e65a7
amitmraval <amitmraval@yahoo.com>
2021-06-11 11:47:50 -0400

    updated port for server.js

--- server.js
+++ server.js
@@ -75,4 +75,8 @@ io.on("connection", (socket) => {
   });
 });
 
-server.listen(4000, () => console.log("server is running on port 4000"));
+app.set("port", process.env.PORT || 4000);
+
+app.listen(app.get("port"), () => {
+  console.log(`PORT: ${app.get("port")}`);
+});
